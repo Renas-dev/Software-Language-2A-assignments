e21abf69ff3752f2429216ff46d2319a9a8f34d7
Renas <Renask.dev@gmail.com>
2024-03-07 21:25:32 +0100

    meow

---
+++ Software-Language-2A-assignments/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Software_Language_2A_assignments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
