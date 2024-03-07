5c6fcf19b9fd4d10d33315abacb28fc93b018c65
Renas <Renask.dev@gmail.com>
2024-03-07 21:41:36 +0100

    meow

--- Software-Language-2A-assignments/obj/Debug/net8.0/Software-Language-2A-assignments.AssemblyInfo.cs
+++ Software-Language-2A-assignments/obj/Debug/net8.0/Software-Language-2A-assignments.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Software-Language-2A-assignments")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e21abf69ff3752f2429216ff46d2319a9a8f34d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Software-Language-2A-assignments")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Software-Language-2A-assignments")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
